21934d99c3220558a4df1136d275e679c3c14ef9
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-23 10:47:11 +0530

    change

---
+++ loop.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int count = 1;
+
+    while(count <= 10){
+        cout<< count <<" ";
+        count << endl++;
+
+    }
+
+cout << endl;
+return 0;
+}
