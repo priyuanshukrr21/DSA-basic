c7dac20a922d2a6c20ff649e3265abbac402dc85
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-24 19:13:41 +0530

    update

--- pattern.cpp
+++ pattern.cpp
@@ -1,14 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
     int n = 3;
-    for(int i=0; i<n; i++) {
-     for (int j=0; j<n; j++)
-     {
-        cout << num << " ";
-        num++ endl;
-     }
-     cout<< "after pattern : " << num << endl;
+    int num = 1;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << num << " ";
+            num++;
+        }
+        cout << endl;
+    }
+
     return 0;
 }
