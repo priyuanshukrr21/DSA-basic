2e3baa93755a4dc2189b8141a7117547d7c87219
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-24 18:02:20 +0530

    update

--- prime.cpp
+++ prime.cpp
@@ -1,2 +1,19 @@
 #include<iostream>
 using namespace std;
+
+int main(){
+    int n = 7;
+    bool isPrime = true;
+    for (int i=2;  i<=n-1; i++){
+    if (n % i == 0) {
+        isPrime = false;
+        break;
+
+    }
+
+
+}
+isPrime -> true -> prime
+isPrime -> false -> non prome
+return 0;
+}
