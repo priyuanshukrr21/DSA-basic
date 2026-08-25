88c3d01e638981b490fda89a636b9c29c6cdbbbc
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-25 18:06:42 +0530

    update

---
+++ .vscode/function.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+void printHello() {
+      cout << "hello\n";
+}
+
+  int main(){
+    printHello();
+
+    int a = 10;
+    int b =5;
+
+    cout << "hello\n";
+    return ;
+  }
---
+++ topdown.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 5;
+
+    // Top half
+    for (int i = 1; i <= n; i++) {
+
+        // spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    // Bottom half
+    for (int i = n - 1; i >= 1; i--) {
+
+        // spaces
+        for (int j = 1; j <= n - i; j++) {
+            cout << " ";
+        }
+
+        // stars
+        for (int j = 1; j <= 2 * i - 1; j++) {
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
