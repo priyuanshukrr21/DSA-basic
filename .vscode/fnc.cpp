2a6cd6941f6e997d289bf657dabe35021862ad56
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-25 18:13:54 +0530

    update

---
+++ .vscode/fnc.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int sum(int a, int b) {
+   int s = a + b;
+   return s;
+
+}
+
+    int sum() {
+        sum (10, 5)<< endl;
+        cout << (10+5)
+        return 0;
+    }
