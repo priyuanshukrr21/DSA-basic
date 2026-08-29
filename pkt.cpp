0550dfe7f9ce3ef4f97f1d3ac610b0434c30d0b1
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-29 08:52:07 +0530

    change code

--- pkt.cpp
+++ pkt.cpp
@@ -3,6 +3,16 @@ using namespace std;
 
 int main() {
     int marks[5] = {99, 100, 54, 36, 88};
+   
+     cout << marks[0] << endl;
+     cout << marks[1] << endl;
+     cout << marks[2] << endl;
+     cout << marks[3] << endl;
+     cout << marks[4] << endl;
+
+
+     cout << marks[-1] << endl;
+
 
 
     return 0;
