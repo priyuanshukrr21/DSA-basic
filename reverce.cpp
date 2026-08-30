df24c44d01aaa9c0920e0026bc35fd9a29e81541
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-30 11:37:34 +0530

    update new

---
+++ reverce.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+while(start < end) {
+    swap(start < end) {
+        start++;
+        end--;
+    }
+}
+int main() {
+    int arr[] = {4,2,7,8,1,2,5};
+    int sz = 7;
+    reversrarray (arr, sz):
+    for (int i=0; i<sz; i++) {
+        cout << arr[i]<< " ";
+
+    }
+    cout << endl;
+    return 0;
+}
---
+++ vector.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> vec;
+    cout << vec[0] << endl;
+    return 0;
+}
