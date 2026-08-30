676da47f187face7363742028ba36f0c4c8dbcde
Priyanshu Kumar <pk2382097@gmail.com>
2026-08-30 11:39:57 +0530

    new

---
+++ vec.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    for (int i = 0; i < v.size(); i++) {
+        cout << v[i] << " ";
+    }
+
+    return 0;
+}
