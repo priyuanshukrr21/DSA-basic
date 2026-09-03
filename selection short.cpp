3c3fcb27299250a552be967d33d6ee80c07e5c3b
Priyanshu Kumar <pk2382097@gmail.com>
2026-09-03 21:01:17 +0530

    update

--- selection short.cpp
+++ selection short.cpp
@@ -3,7 +3,7 @@ using namespace std;
 for (int i=0; i<n-1; i++) {
     int minIndex = i;
     for (int j=i+1; j<n; j++) {
-        if (arr[j] < arr[minIndex]) {
+        if (arr[j] <arr[minIndex]) {
             minIndex = j;
         }
     }
