e499e60af0608cde376e26f7cca62e2b44949a9a
Priyanshu Kumar <pk2382097@gmail.com>
2026-09-03 20:22:49 +0530

    updatenew

---
+++ binarysearch.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int s=0, e=n-1;
+while(s<=e){
+    int mid = s + (e-s)/2;
+    if(arr[mid] == target){
+        return mid;
+    }
+    else if(arr[mid] < target){
+        s = mid + 1;
+    }
+    else{
+        e = mid - 1;
+    }
+}
+
+
