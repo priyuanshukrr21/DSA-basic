c7a6a8bfdf5fdfabc4f97dfd1f01e46a639ce123
Priyanshu Kumar <pk2382097@gmail.com>
2026-09-10 19:43:22 +0530

    update

--- up.cpp
+++ up.cpp
@@ -6,10 +6,9 @@ using namespace std;
 int main() {
 
 int arr[]= {1, 2, 3, 4, 5};
-int a = 10;
-int* ptr = &a;
-cout << ptr << endl;
-ptr = ptr + 2;
-cout << *ptr << endl;
+int* ptr2;
+int* ptr1; = ptr2 + 2;
+
+cout << ptr1 - ptr2 << endl;
 return 0;
 }
