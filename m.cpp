dbf8fc9afd5f7424ead2f084d422eaf1352c9caa
Priyanshu Kumar <pk2382097@gmail.com>
2026-09-23 16:26:55 +0530

    new

---
+++ m.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+
+    Node(int value) {
+        data = value;
+        next = nullptr;
+    }
+};
+
+Node* reverseList(Node* head) {
+    Node* prev = nullptr;
+    Node* current = head;
+
+    while (current != nullptr) {
+        Node* nextNode = current->next;
+
+        current->next = prev;
+
+        prev = current;
+        current = nextNode;
+    }
+
+    return prev;
+}
+
+void printList(Node* head) {
+    while (head != nullptr) {
+        cout << head->data << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    Node* head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+
+    cout << "Original List: ";
+    printList(head);
+
+    head = reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(head);
+
+    return 0;
+}
